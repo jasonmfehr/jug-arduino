df29cc0a58c71604ae3c21e511832bbc3337ab18
Jason Fehr <jasonmfehr@server.fake>
2014-09-03 22:14:20 -0500

    adding necessary transfer object

---
+++ java-server/src/main/java/com/jfehr/jug/iot/data/RemoteBoardInputDataTO.java
@@ -0,0 +1,22 @@
+package com.jfehr.jug.iot.data;
+
+public class RemoteBoardInputDataTO {
+
+	private String remoteBoardIP;
+	private Integer remoteBoardPort;
+	
+	public String getRemoteBoardIP() {
+		return remoteBoardIP;
+	}
+	public void setRemoteBoardIP(String remoteBoardIP) {
+		this.remoteBoardIP = remoteBoardIP;
+	}
+	
+	public Integer getRemoteBoardPort() {
+		return remoteBoardPort;
+	}
+	public void setRemoteBoardPort(Integer remoteBoardPort) {
+		this.remoteBoardPort = remoteBoardPort;
+	}
+	
+}
