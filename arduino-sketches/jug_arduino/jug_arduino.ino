573c37393bc6a3db5267f76246b542c4819455d3
Jason Fehr <jasonmfehr@server.fake>
2014-09-08 21:20:38 -0500

    refactored code to shrink its size and make it easier to read

--- arduino-sketches/jug_arduino/jug_arduino.ino
+++ arduino-sketches/jug_arduino/jug_arduino.ino
@@ -3,29 +3,34 @@
 
 const unsigned int SERVER_PORT = 51420;
 
-//constants for commands
+//constants for command numbers which are sent as the first byte
 const byte CMD_ECHO = 0;
-const byte CMD_ECHO_DATA_BYTE_COUNT = 2;
 const byte CMD_SET_LED = 1;
-const byte CMD_SET_LED_DATA_BYTE_COUNT = 2;
 const byte CMD_READ_TEMP = 2;
-const byte CMD_READ_TEMP_DATA_BYTE_COUNT = 0;
 const byte CMD_SET_COLOR_LED = 3;
+
+//constants for the number of data bytes that follow the initial command byte
+const byte CMD_ECHO_DATA_BYTE_COUNT = 2;
+const byte CMD_SET_LED_DATA_BYTE_COUNT = 2;
+const byte CMD_READ_TEMP_DATA_BYTE_COUNT = 0;
 const byte CMD_SET_COLOR_LED_DATA_BYTE_COUNT = 3;
+
+//convenience array mapping the command numbers to their respective data byte counts
+const byte COMMAND_TO_DATA_BYTE_COUNT_MAP[4] = {CMD_ECHO_DATA_BYTE_COUNT, CMD_SET_LED_DATA_BYTE_COUNT, CMD_READ_TEMP_DATA_BYTE_COUNT, CMD_SET_COLOR_LED_DATA_BYTE_COUNT};
 
 //indicies of data bytes for the SET_LED command
 const byte LED_NUMBER_INDEX = 0;
 const byte LED_ONOFF_INDEX = 1;
 const byte LED_SET_OFF = 0;
 
-//array mapping led numbers to the pin they are on
+//array mapping led numbers to the I/O pin they are on
 const byte LED_PINS[4] = {6, 7, 8, 9};
 
 //analog pin to which the temperature sensor is connected
 const byte TEMP_INPUT_PIN = 0;
 const float ANALOG_STEP = 0.0049;
 
-//pin mappings for the multi-color LED
+//pin mappings for the PWM output pins that drive the multi-color LED
 const byte RED_PIN = 2;
 const byte GREEN_PIN = 3;
 const byte BLUE_PIN = 4;
@@ -35,9 +40,10 @@ const byte RED_COLOR_INDEX = 0;
 const byte GREEN_COLOR_INDEX = 1;
 const byte BLUE_COLOR_INDEX = 2;
 
-//number of miiliseconds to wait for the client to start sending bytes
+//number of miiliseconds to wait for the client to start sending bytes before timing out
 unsigned long CLIENT_TIMEOUT = 10000;
 
+//setup of the ethernet shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 byte ip[] = { 192, 168, 1, 7};
 byte gateway[] = { 192, 168, 1, 1 };
@@ -63,6 +69,7 @@ void loop() {
   EthernetClient client = server.available();
   byte commandByte;
   byte* dataBytes;
+  byte dataBytesCount;
   
   if(client){
     Serial.println("\nclient connected");
@@ -72,23 +79,33 @@ void loop() {
     Serial.print("got command byte: ");
     Serial.println(commandByte);
     
-    dataBytes = readDataBytes(&client, determineNumberOfDataBytes(commandByte));
+    //default to zero for unknown commands
+    if(commandByte >= 0 && commandByte < (sizeof(COMMAND_TO_DATA_BYTE_COUNT_MAP) / sizeof(byte))){
+      dataBytesCount = COMMAND_TO_DATA_BYTE_COUNT_MAP[commandByte];
+      Serial.print("reading number of data bytes: ");
+      Serial.println(dataBytesCount);
+      dataBytes = readDataBytes(&client, dataBytesCount);
+    }
     
     switch(commandByte){
       case CMD_ECHO:
+        Serial.println("command ECHO");
         handleEchoCommand(&client, dataBytes);
         break;
       case CMD_SET_LED:
+        Serial.println("command SET_LED");
         handleSetLedCommand(dataBytes);
         break;
       case CMD_READ_TEMP:
+        Serial.println("command READ_TEMP");
         handleReadTempCommand(&client);
         break;
       case CMD_SET_COLOR_LED:
+        Serial.println("command SET_COLOR_LED");
         handleSetColorLedCommand(dataBytes);
         break;
       default:
-        Serial.print("Unknown command: ");
+        Serial.print("unknown command: ");
         Serial.println(commandByte);
         break;    
     };
@@ -99,37 +116,10 @@ void loop() {
   }
 }
 
-byte determineNumberOfDataBytes(byte commandByte){
-  byte num;
-  
-  switch(commandByte){
-    case CMD_ECHO:
-      Serial.println("command ECHO");
-      num = CMD_ECHO_DATA_BYTE_COUNT;
-      break;
-    case CMD_SET_LED:
-      Serial.println("command SET_LED");
-      num = CMD_SET_LED_DATA_BYTE_COUNT;
-      break;
-    case CMD_READ_TEMP:
-      Serial.println("command READ_TEMP");
-      num = CMD_READ_TEMP_DATA_BYTE_COUNT;
-      break;
-    case CMD_SET_COLOR_LED:
-      Serial.println("command SET_COLOR_LED");
-      num = CMD_SET_COLOR_LED_DATA_BYTE_COUNT;
-      break;
-    default:
-      Serial.print("Unknown command: ");
-      Serial.println(commandByte);
-      num = 0;
-      break;    
-  };
-  
-  Serial.print("number of data bytes: ");
-  Serial.println(num);
-  
-  return num;
+void handleEchoCommand(EthernetClient* client, byte* data) {
+  Serial.println("echoing bytes to client");
+  client->write(data, CMD_ECHO_DATA_BYTE_COUNT);
+  Serial.println("done echoing bytes to client");
 }
 
 void handleSetLedCommand(byte* data) {
@@ -148,14 +138,18 @@ void handleSetLedCommand(byte* data) {
   }
 }
 
-void handleEchoCommand(EthernetClient* client, byte* data) {
-  Serial.println("echoing bytes to client");
-  client->write(data, CMD_ECHO_DATA_BYTE_COUNT);
-  Serial.println("done echoing bytes to client");
-}
-
 void handleReadTempCommand(EthernetClient* client) {
-  byte temp = readTemp();
+  byte temp;
+  float readVoltage;
+  
+  //the voltage is returned as a number from 0 to 1023 with each number representing 0.0049 volts, 
+  //multiply to get the actual voltage that is read
+  readVoltage = analogRead(TEMP_INPUT_PIN) * ANALOG_STEP;
+  Serial.print("read voltage: ");
+  Serial.println(readVoltage);
+  
+  //a reading of 0.5 volts indicates 0 degrees celsius
+  temp = (readVoltage - 0.5) * 100;
   
   Serial.print("read temperature of ");
   Serial.println(temp);
@@ -176,6 +170,16 @@ void handleSetColorLedCommand(byte* data) {
   analogWrite(BLUE_PIN, data[BLUE_COLOR_INDEX]);
 }
 
+/**
+ * reads a specified number of bytes that are sent from the client and returns an array with those bytes
+ * assumes the caller of this method knows how big the array will be
+ *
+ * client - pointer to the client connection object
+ * numberOfDataBytes - count of bytes to read
+ *
+ * return - byte array created using the new command, this byte array 
+ *          must be explicitly deleted or it will cause a memory leak
+ */
 byte* readDataBytes(EthernetClient* client, byte numberOfDataBytes) {
   unsigned long waitStartTime = millis();
   byte inputByte;
@@ -186,6 +190,7 @@ byte* readDataBytes(EthernetClient* client, byte numberOfDataBytes) {
   Serial.println(numberOfDataBytes);
   
   while(bytesRead < numberOfDataBytes && (millis() - waitStartTime < CLIENT_TIMEOUT)){
+    //since client->read() is a non-blocking call, check to see if bytes are available for reading before attempting a read
     if(client->available()){
       inputByte = client->read();
       Serial.print("read byte: ");
@@ -199,17 +204,6 @@ byte* readDataBytes(EthernetClient* client, byte numberOfDataBytes) {
   }
   
   return dataByteArray;
-}
-
-byte readTemp() {
-  //the voltage is returned as a number from 0 to 1023 with each number representing 0.0049 volts, 
-  //multiply to get the actual voltage that is read
-  float readVoltage = analogRead(TEMP_INPUT_PIN) * ANALOG_STEP;
-  Serial.print("read voltage: ");
-  Serial.println(readVoltage);
-  
-  //a reading of 0.5 volts indicates 0 degrees celsius
-  return (readVoltage - 0.5) * 100;
 }
 
 void setupLEDs() {
